90cad70bce0fb87533754725b5f26be33b0ebc3a
ivo <ipooters@gmail.com>
2015-04-02 12:26:40 +0200

    changed default config

--- qotdweb/default_config.py
+++ qotdweb/default_config.py
@@ -2,4 +2,8 @@ __author__ = 'ivo'
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG=True
+TESTING=False
 SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(os.path.join(basedir, "qotdweb.db"))
+SECRET_KEY = "REPLACE THIS KEY!"

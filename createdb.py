172c4c889c9fd5d5ba85941050e47b5afb542458
ivo <ipooters@gmail.com>
2015-04-23 20:15:47 +0200

    added db arg to createdb.py

--- createdb.py
+++ createdb.py
@@ -14,9 +14,10 @@ log = logging.getLogger()
 def main():
     logging.basicConfig(level=logging.DEBUG)
     argparser = argparse.ArgumentParser(description="Create qotd database")
+    argparser.add_argument("db", help="The connection string. E.g sqlite:////tmp/qotd.db")
     args = argparser.parse_args()
     from qotdweb import app, db, models
-
+    app.config["SQLALCHEMY_DATABASE_URI"] = args.db
     db.create_all()
 
 if __name__ == "__main__":
